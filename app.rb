a947cf6f05bf55103455697495a695355c92039a
Adam Legg <adam.legg@gmail.com>
2014-07-30 14:02:57 -0600

    modified current_user method

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require "sinatra"
 require "gschool_database_connection"
 require "rack-flash"
+require "./lib/user"
+require "./lib/fish"
 
 class App < Sinatra::Application
   enable :sessions
@@ -14,7 +16,10 @@ class App < Sinatra::Application
   get "/" do
     user = current_user
 
+
+
     if current_user
+      # users = User.find(:id != :user_id)
       users = @database_connection.sql("SELECT * FROM users WHERE id != #{user["id"]}")
       fish = @database_connection.sql("SELECT * FROM fish WHERE user_id = #{current_user["id"]}")
       erb :signed_in, locals: {current_user: user, users: users, fish_list: fish}
@@ -184,12 +189,7 @@ class App < Sinatra::Application
 
   def current_user
     if session[:user_id]
-      select_sql = <<-SQL
-      SELECT * FROM users
-      WHERE id = #{session[:user_id]}
-      SQL
-
-      @database_connection.sql(select_sql).first
+      User.find(session[:user_id])
     else
       nil
     end
