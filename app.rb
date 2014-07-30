ac275bb30afdd1ee8c954a9c1e0205383b68ec3c
Adam Legg <adam.legg@gmail.com>
2014-07-30 14:26:52 -0600

    updated post fish

--- app.rb
+++ app.rb
@@ -20,6 +20,7 @@ class App < Sinatra::Application
 
     if current_user
       # users = User.find(:id != :user_id)
+
       users = @database_connection.sql("SELECT * FROM users WHERE id != #{user["id"]}")
       fish = @database_connection.sql("SELECT * FROM fish WHERE user_id = #{current_user["id"]}")
       erb :signed_in, locals: {current_user: user, users: users, fish_list: fish}
@@ -78,15 +79,8 @@ class App < Sinatra::Application
 
   post "/fish" do
     if validate_fish_params
-      insert_sql = <<-SQL
-      INSERT INTO fish (name, wikipedia_page, user_id)
-      VALUES ('#{params[:name]}', '#{params[:wikipedia_page]}', #{current_user["id"]})
-      SQL
-
-      @database_connection.sql(insert_sql)
-
+      Fish.create(:name => (params[:name]), :wikipedia_page => (params[:wikipedia_page]), :user_id => (current_user[:id]))
       flash[:notice] = "Fish Created"
-
       redirect "/"
     else
       erb :"fish/new"
